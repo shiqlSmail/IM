646c817f7e4740e7159ba01d2b84cce3bd8f8f0a
jackSmail <jack1053996819@163.com>
2020-06-20 22:41:21 +0800

    新增签名校验，IP校验

--- im/esb-server/src/main/java/com/esb/im/server/controller/ESBController.java
+++ im/esb-server/src/main/java/com/esb/im/server/controller/ESBController.java
@@ -5,13 +5,23 @@ import com.esb.im.server.domain.IMApiDO;
 import com.esb.im.server.service.IMApiService;
 import com.esb.im.server.system.InterfaceBean;
 import com.esb.im.server.xml.AnalysisFactory;
+import com.server.tools.crypto.SM2;
+import com.server.tools.crypto.Util;
 import com.server.tools.result.InterfaceResultData;
+import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.dom4j.DocumentException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.spring.web.json.Json;
 
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -21,10 +31,19 @@ public class ESBController extends InterfaceBean {
     @Autowired(required = false)
     private IMApiService ilonwApiService;
 
+    @Value("${app_ip}")
+    private String propertiesAppIp;
+
     @CrossOrigin(origins = "*", maxAge = 3600)
     @RequestMapping(value = "/xml",method = RequestMethod.POST)
     public String xml(@RequestBody String xml){
+        //先解密
+        Map<String,String> maps = getAppInfo(xml);
+        if(StringUtils.equals(maps.get("code"),"SIGN-999999")){
+            return JSON.toJSONString(maps);
+        }
         try {
+            xml = maps.get("data");
             log.info("请求的xml信息为："+xml);
             //解析XML
             Map map = AnalysisFactory.parseXml(xml);
@@ -34,6 +53,13 @@ public class ESBController extends InterfaceBean {
             Map<String,Object> appHeadMap = net.sf.json.JSONObject.fromObject(headMap.get("HEAD"));
             Map<String,Object> resultMap = net.sf.json.JSONObject.fromObject(appHeadMap.get("SYS_HEAD"));
 
+            String appIp = String.valueOf(resultMap.get("APP_IP"));
+            if(!propertiesAppIp.contains(appIp)){
+                Map<String, String> ipMaps = new HashMap<String, String>();
+                ipMaps.put("data", "非法请求");
+                ipMaps.put("code", "ILLEGAL-REQUEST");
+                return JSON.toJSONString(ipMaps);
+            }
             String transCode = String.valueOf(resultMap.get("SYS_CHANNEL_ID"));
             IMApiDO ilonwApiDO = ilonwApiService.selectByTransCode(transCode);
 
@@ -53,7 +79,13 @@ public class ESBController extends InterfaceBean {
     @CrossOrigin(origins = "*", maxAge = 3600)
     @RequestMapping(value = "/json",method = RequestMethod.POST)
     public String json(@RequestBody String json){
+        //先解密
+        Map<String,String> maps = getAppInfo(json);
+        if(StringUtils.equals(maps.get("code"),"SIGN-999999")){
+            return JSON.toJSONString(maps);
+        }
         try {
+            json = maps.get("data");
             Map<String,Object> map = (Map) JSON.parse(json);
             String transCode = map.get("trans_code").toString();
             IMApiDO ilonwApiDO = ilonwApiService.selectByTransCode(transCode);
@@ -68,5 +100,28 @@ public class ESBController extends InterfaceBean {
             e.printStackTrace();
             return null;
         }
+    }
+
+    private Map<String, String> getAppInfo(String sign) {
+        Map<String, String> map = new HashMap<String, String>();
+        try {
+            //调用解密方法
+            SM2 sm02 = new SM2();
+            log.info("-----------------开始进行解密操作-----------------");
+           // BigInteger privateKey = sm02.importPrivateKey("/usr/src/certificate/privatekey.pem");
+            BigInteger privateKey = sm02.importPrivateKey("H:\\crypto\\esb-privatekey.pem");
+            byte[] data = Util.hexStringToBytes(sign);
+            String appInfo = sm02.decrypt(data, privateKey);
+            log.info("解密完成的数据为：" + appInfo);
+            map.put("data", appInfo);
+            map.put("code", "sign_success");
+            log.info("-----------------结束解密操作-----------------");
+        } catch (Exception e) {
+            log.info("解密异常：" + e.getMessage());
+            map.put("data", "签名异常");
+            map.put("code", "SIGN-999999");
+            e.printStackTrace();
+        }
+        return map;
     }
 }
--- im/im-server-tools/src/main/java/com/server/tools/crypto/SM2.java
+++ im/im-server-tools/src/main/java/com/server/tools/crypto/SM2.java
@@ -789,15 +789,15 @@ public class SM2 {
 		 "0AE4C779 8AA0F119 471BEE11 825BE462 02BB79E2 A5844495 E97C04FF  4DF2548A".replace(" ", ""), 16);
 		 BigInteger py = new BigInteger( "7C0240F8 8F1CD4E1 6352A73C 17B7F16F 07353E53 A176D684 A9FE0C6B  B798E857".replace(" ", ""), 16);
 		 ECPoint publicKey = sm02.curve.createPoint(px, py);
-		 BigInteger privateKey = new BigInteger( "128B2FA8 BD433C6C 068C8D80 3DFF7979 2A519A55 171B1B65 0C23661D 15897263".replace(" ", ""), 16);*/
-
-		 /*SM2KeyPair keyPair = sm02.generateKeyPair();
+		 BigInteger privateKey = new BigInteger( "128B2FA8 BD433C6C 068C8D80 3DFF7979 2A519A55 171B1B65 0C23661D 15897263".replace(" ", ""), 16);
+*/
+		 SM2KeyPair keyPair = sm02.generateKeyPair();
 		 ECPoint publicKey=keyPair.getPublicKey();
 		 BigInteger privateKey=keyPair.getPrivateKey();
-		 sm02.exportPublicKey(publicKey, "E:/publickey.pem");
-		 sm02.exportPrivateKey(privateKey, "E:/privatekey.pem");*/
+		 sm02.exportPublicKey(publicKey, "H:\\crypto\\esb-publickey.pem");
+		 sm02.exportPrivateKey(privateKey, "H:\\crypto\\esb-privatekey.pem");
 
-		System.out.println("-----------------公钥加密与解密-----------------");
+		/*System.out.println("-----------------公钥加密与解密-----------------");
 		ECPoint publicKey = sm02.importPublicKey("E:/publickey.pem");
 		BigInteger privateKey = sm02.importPrivateKey("E:/privatekey.pem");
 		byte[] data = sm02.encrypt("helloWorld", publicKey);
@@ -830,7 +830,7 @@ public class SM2 {
 
 
 		System.out.println("解密后明文========:" + publicKey.getEncoded());
-		System.out.println("解密后明文:===========" + publicKey.getEncoded());
+		System.out.println("解密后明文:===========" + publicKey.getEncoded());*/
 	}
 
 	public static class Signature {
